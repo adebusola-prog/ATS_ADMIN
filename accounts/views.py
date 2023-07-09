dea41ff19b74e5b9948de100550536e052380e7d
zeu <aadeyeye@afexnigeria.com>
2023-07-09 19:24:48 +0100

    added more

--- accounts/views.py
+++ accounts/views.py
@@ -111,4 +111,6 @@ class SetNewPasswordView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"status": "success", "message": "Password was successfully reset"}, status=status.HTTP_200_OK)
-    
+
+    def perform_update(self, serializer):
+        serializer.save()
