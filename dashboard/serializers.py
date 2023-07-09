bbf9024fb48d2a4b77e9a55a9b811d32321ba2bb
zeu <aadeyeye@afexnigeria.com>
2023-07-09 02:36:08 +0100

    added more

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -9,18 +9,18 @@ class ActivityLogSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = ActivityLog
-        fields = ('actor','action_type','get_timesince', 'status', 'content_type','data')
+        fields = ('action_type','get_timesince', 'status', 'content_type','data')
 
-    def get_actor(self, obj):
-        actor = obj.posted_by.actor
-        if actor is not None:
-            user_id = actor.id
-            user = CustomUser.objects.get(id=user_id)
-            serializer = CustomUserSubAdminSerializer(user, context=self.context)
-            first_name = serializer.data['first_name']
-            last_name = serializer.data['last_name']
-            return first_name + ' ' + last_name
-        return None
+    # def get_actor(self, obj):
+    #     actor = obj.posted_by.actor
+    #     if actor is not None:
+    #         user_id = actor.id
+    #         user = CustomUser.objects.get(id=user_id)
+    #         serializer = CustomUserSubAdminSerializer(user, context=self.context)
+    #         first_name = serializer.data['first_name']
+    #         last_name = serializer.data['last_name']
+    #         return first_name + ' ' + last_name
+    #     return None
 
     def get_content_type(self, obj):
         content_type = obj.content_type
