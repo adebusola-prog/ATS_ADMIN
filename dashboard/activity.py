fd213e17e13dca760c40db5706c7bedd6fac4d6b
zeu <aadeyeye@afexnigeria.com>
2023-07-09 14:23:14 +0100

    added more

--- dashboard/activity.py
+++ dashboard/activity.py
@@ -80,9 +80,10 @@ class ActivityLogJobMixin:
         message = f"New job created by {instance.posted_by.first_name} {instance.posted_by.last_name}"
         ActivityLog.objects.create(actor=actor, action_type=CREATE, content_object=instance, data=message)
 
-    def _update_activity_log(self, instance, updated_instance, request):
+    def _update_activity_log(self, instance, request):
         actor = self._get_user(request)
-        message = f"{instance.role} updated by {instance.posted_by.first_name} {instance.posted_by.last_name}"
+        old_instance = self.queryset.get(pk=instance.pk)
+        message = f"{old_instance.role} updated by {instance.posted_by.first_name} {instance.posted_by.last_name}"
         ActivityLog.objects.create(actor=actor, action_type=UPDATE, content_object=instance, data=message)
 
     def _delete_activity_log(self, instance, request):
