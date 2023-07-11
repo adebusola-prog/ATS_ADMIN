be0e1ced686e264c553cd3f180e401452a3daf39
zeu <aadeyeye@afexnigeria.com>
2023-07-11 16:06:41 +0100

    added shortlist

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import LoginView, LogoutView, ForgotPasswordView, \
-    ResetPasswordView, SetNewPasswordView, PermissionLevelListView
+    ResetPasswordView, SetNewPasswordView, PermissionLevelListAPIView
 
 
 app_name = "authe"
 
 urlpatterns = [
-        path('permission_level', PermissionLevelListView.as_view(), name='permission_level'),    
+        path('permission_level', PermissionLevelListAPIView.as_view(), name='permission_level'),    
         path('login', LoginView.as_view(), name='log_in'), 
         path('logout', LogoutView.as_view(), name='log_out'),
         path('forgot_password', ForgotPasswordView.as_view(),\
