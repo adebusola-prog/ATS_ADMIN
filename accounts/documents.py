d57756209f76ac344108f64406e4a192d179a60f
zeu <aadeyeye@afexnigeria.com>
2023-07-11 01:51:38 +0100

    added sub_admin_profile

--- accounts/documents.py
+++ accounts/documents.py
@@ -1,26 +1,26 @@
-from django_elasticsearch_dsl import Document, fields
-from django_elasticsearch_dsl.registries import registry
-from .models import CustomUser
+# from django_elasticsearch_dsl import Document, fields
+# from django_elasticsearch_dsl.registries import registry
+# from .models import CustomUser
 
-@registry.register_document
-class CustomUserDocument(Document):
-    first_name = fields.TextField(
-        attr='first_name',
-        fields={
-         'raw': fields.TextField(),
-         'suggest': fields.CompletionField(),
-     }
-    )
-    last_name = fields.TextField(
-        attr='last_name',
-        fields={
-         'raw': fields.TextField(),
-         'suggest': fields.CompletionField(),
-     }
-        )
+# @registry.register_document
+# class CustomUserDocument(Document):
+#     first_name = fields.TextField(
+#         attr='first_name',
+#         fields={
+#          'raw': fields.TextField(),
+#          'suggest': fields.CompletionField(),
+#      }
+#     )
+#     last_name = fields.TextField(
+#         attr='last_name',
+#         fields={
+#          'raw': fields.TextField(),
+#          'suggest': fields.CompletionField(),
+#      }
+#         )
     
-    class Index:
-        name = 'users'  
+#     class Index:
+#         name = 'users'  
 
-    class Django:
-        model = CustomUser 
+#     class Django:
+#         model = CustomUser 
