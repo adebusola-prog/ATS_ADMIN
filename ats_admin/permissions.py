21a8a1d891792da08599ad2cb253a84d35b75b77
zeu <aadeyeye@afexnigeria.com>
2023-07-13 14:14:17 +0100

    add

--- ats_admin/permissions.py
+++ ats_admin/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import PermissionDenied
 
 class IsSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and request.user.is_superadmin:
             return True
         raise PermissionDenied("You are not a superadmin!!. You are not allowed to perform this operation.")
 
