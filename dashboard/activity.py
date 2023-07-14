5854730e0632d508d2051f1a458894a5140b2a8c
zeu <aadeyeye@afexnigeria.com>
2023-07-14 11:19:38 +0100

    added more

--- dashboard/activity.py
+++ dashboard/activity.py
@@ -34,5 +34,5 @@ class ActivityLogJobMixin:
 
     def _create_application_activity_log(self, instance, request):
         actor = self._get_user(request)
-        message = f"New job Application by {instance.posted_by.first_name} {instance.posted_by.last_name}"
+        message = f"New job Application by {instance.applicant.first_name} {instance.applicant.last_name}"
         ActivityLog.objects.create(actor=actor, action_type=CREATE, content_object=instance, data=message)
